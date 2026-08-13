ccd7bf4af20fb0f982793ac24be02815bec61d1b
Hank Xiao <a845656974@outlook.com>
2026-08-13 14:25:13 +0800

    update

---
+++ STM32C542R_Project5/STM32C542R_Project5_cmake/build/debug_GCC_STM32C542RCT6/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,9 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "debug_GCC_STM32C542RCT6")
+  file(REMOVE_RECURSE
+  "STM32C542R_Project5.elf"
+  "STM32C542R_Project5.map"
+  )
+endif()
